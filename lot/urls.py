d6e0fb0fea7e3e5e63eb1a39aca17659b42aeee8
denisdolmatov2020 <denisdolmatov2020@gmail.com>
2020-09-11 12:39:35 +0700

    update lots and numbers

--- lot/urls.py
+++ lot/urls.py
@@ -11,11 +11,10 @@ urlpatterns = [
     path('api/lots/', include('lots.urls')),
     path('api/number/', include('number.urls')),
     path('api/my-user/', include('my_user.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
+    path('api/rest-auth/', include('rest_auth.urls')),
     path('api/user/', include('djoser.urls')),
-    path('auth/', include('rest_framework_social_oauth2.urls')),
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('api/auth/', include('rest_framework_social_oauth2.urls')),
+    path('api/o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
